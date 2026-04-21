a28cc38a05d90d5efafc9ce1096d6c22d0db2a76
oyhampe1992-design <oyhampe1992@gmail.com>
2026-04-21 12:37:18 +0200

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -26,9 +26,9 @@ export default async function handler(req, res) {
       })
     });
 
-    const data = await response.json();
-    return res.status(response.status).json(data);
+    const data = await response.text();
+    return res.status(response.status).send(data);
   } catch (err) {
-    return res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: 'Server error: ' + err.message });
   }
-}
+};
